b30c9cba033338260a374571dff818cd6091af6e
ferrariagustin <agustin.ferrari@mercadolibre.com>
2024-06-22 21:25:35 -0300

    add motion layout on splashView

--- app/src/main/java/com/aferrari/trailead/app/ui/SplashActivity.kt
+++ app/src/main/java/com/aferrari/trailead/app/ui/SplashActivity.kt
@@ -4,8 +4,11 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.aferrari.trailead.R
 import com.aferrari.trailead.app.viewmodel.login.LoginViewModelFactory
 import com.aferrari.trailead.app.viewmodel.login.SplashViewModel
 import com.aferrari.trailead.domain.datasource.RemoteDataSource
@@ -22,12 +25,20 @@ class SplashActivity : AppCompatActivity() {
     private lateinit var splashViewModel: SplashViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         super.onCreate(savedInstanceState)
-        val factory = LoginViewModelFactory(remoteDataSource)
-        splashViewModel = ViewModelProvider(this, factory)[SplashViewModel::class.java]
-        val loginActivity = Intent(Intent.ACTION_VIEW, Uri.parse("trailead://login"))
-        this.startActivity(loginActivity)
-        finish()
+        setContentView(R.layout.motion_layout)
+
+        Handler().postDelayed({
+            val factory = LoginViewModelFactory(remoteDataSource)
+            splashViewModel = ViewModelProvider(this, factory)[SplashViewModel::class.java]
+            val loginActivity = Intent(Intent.ACTION_VIEW, Uri.parse("trailead://login"))
+            this.startActivity(loginActivity)
+            finish()
+        }, 2000)
     }
 
 }
